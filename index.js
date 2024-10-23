eba8d64e2df21d68681e7ad4c9345c95bc598e00
smartsiva <52667587+smartsiva@users.noreply.github.com>
2024-10-23 22:50:28 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,31 @@
+const puppeteer = require('puppeteer');
+
+exports.handler = async (event) => {
+  try {
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
+
+    const page = await browser.newPage();
+    await page.goto('https://example.com');
+
+    const screenshotBuffer = await page.screenshot({ encoding: 'binary' });
+
+    await browser.close();
+
+    return {
+      statusCode: 200,
+      body: screenshotBuffer.toString('base64'),
+      headers: {
+        'Content-Type': 'image/png',
+        'Content-Disposition': 'attachment; filename="screenshot.png"',
+      },
+    };
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
