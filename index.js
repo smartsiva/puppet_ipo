fb64be056b41e86f9e06f8a43639a53029dc31f4
smartsiva <52667587+smartsiva@users.noreply.github.com>
2024-10-23 22:52:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ exports.handler = async (event) => {
     });
 
     const page = await browser.newPage();
-    await page.goto('https://example.com');
+    await page.goto('https://example1.com');
 
     const screenshotBuffer = await page.screenshot({ encoding: 'binary' });
 
